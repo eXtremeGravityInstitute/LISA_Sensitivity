f56a17dac20f454c112d8d5439729e74e57c6896
Travis Robson <travis.robson@ligo.org>
2018-03-02 12:17:23 -0700

    Cleaned up a lot

--- LISA_tools.py
+++ LISA_tools.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import optimize
 
-""" PhenomA coefficeints """
+"""
+PhenomA coefficeints:
+Coefficients from Table 2 in ``LISA Sensitivity'' - Neil Cornish & Travis Robson
+"""
 
 a0 = 2.9740e-1
 b0 = 4.4810e-2
@@ -22,25 +24,27 @@ c3 = 2.7299e-1
 
 """ Constants """
 C       = 299792458.         # m/s
-YEAR    = 3.15581497632e7 # sec
+YEAR    = 3.15581497632e7    # sec
 TSUN    = 4.92549232189886339689643862e-6 # mass of sun in seconds (G=C=1)
-H0      = 69.6           # Hubble's parameter today
-Omega_m = 0.286  # density parameter of dust matter
-MPC     = 3.08568025e22 # mega-Parsec in meters
+MPC     = 3.08568025e22/C    # mega-Parsec in seconds
 
-""" LISA arm length """
-L = 2.5e9 # meters
 
-""" Transfer Frequency """
-f_star = C/(2.*np.pi*L)
+def get_Sn(f, constants):
+	# constants = np.arrays([H0, Omega_m, L, f_star, Tobs, NC])
+	f_star = constants[3]
+	L      = constants[2]
+	NC     = constants[5]
+	Tobs   = constants[4]
+	
+	transfer_data = np.genfromtxt('R.txt')
+	f = transfer_data[:,0]*f_star # convert to frequency
+	R = transfer_data[:,1]*NC     # response gets improved by more data channels
+	
+	Sn = get_Pn(f, f_star, L)/R + get_Sc_est(f, Tobs, NC)
+	
+	return Sn
 
-""" Observation Period """
-Tobs = 4.*YEAR
-
-""" Number of Michelson Data Channels """
-NC = 2
-
-def get_Sc_est(f, Tobs):
+def get_Sc_est(f, Tobs, NC):
     """
     Get an estimation of the galactic binary confusion noise are available for
         Tobs = {0.5 yr, 1 yr, 2 yr, 4yr}
@@ -100,7 +104,7 @@ def get_Sc_est(f, Tobs):
     
     return Sc
     
-def get_Pn(f):
+def get_Pn(f, f_star, L):
     """
     Get the Power Spectral Density
     """
@@ -117,45 +121,50 @@ def get_Pn(f):
     return Pn
     
     
-def get_Sn_approx(f):
+def get_Sn_approx(f, f_star, L, NC):
     """
-    Get the noise curve approximation, Equation (1) of ``LISA Sensitivity'' -Neil Cornish
+    Get the noise curve approximation, Equation (1) of ``LISA Sensitivity'' - Neil Cornish & Travis Robson
     """
     
     # Sky and polarization averaged signal response of the detector, Equation (9)
     Ra = 3./20./(1. + 6./10.*(f/f_star)**2)*NC
     
     # strain spectral density, Equation (2)
-    Sn = get_Pn(f)/Ra
+    Sn = get_Pn(f, f_star, L)/Ra
     
     return Sn
     
-  
+
 def get_A(f, M, eta, Mc, Dl):
-    
-    f0 = (a0*eta**2 + b0*eta + c0)/(np.pi*M) # merger frequency
-    f1 = (a1*eta**2 + b1*eta + c1)/(np.pi*M) # ringdown frequency
-    f2 = (a2*eta**2 + b2*eta + c2)/(np.pi*M) # decay-width of ringdown
-    f3 = (a3*eta**2 + b3*eta + c3)/(np.pi*M) # cut-off frequency
-    
-    A = np.sqrt(5./24.)*Mc**(5./6.)*f0**(-7./6.)/np.pi**(2./3)/(Dl/C)
+	"""
+	PhenomA: Binary Waveform
+	--------------
+	Section 2 of ``LISA Sensitivity'' - Neil Cornish & Travis Robson
+	"""
 
-    if (f < f0):
-        A *= (f/f0)**(-7./6.)
-    
-    elif (f0 <= f and f < f1):
-        A *= (f/f0)**(-2./3.)
-    
-    elif (f1 <= f and f < f3):
-        w = 0.5*np.pi*f2*(f0/f1)**(2./3.)
-        A *= w*f2/((f - f1)**2 + 0.25*f2**2)/(2.*np.pi)
-        
-    else:
-        A *= 0.
-    
-    return A
+	f0 = (a0*eta**2 + b0*eta + c0)/(np.pi*M) # merger frequency
+	f1 = (a1*eta**2 + b1*eta + c1)/(np.pi*M) # ringdown frequency
+	f2 = (a2*eta**2 + b2*eta + c2)/(np.pi*M) # decay-width of ringdown
+	f3 = (a3*eta**2 + b3*eta + c3)/(np.pi*M) # cut-off frequency
 
-def get_Dl(z):
+	A = np.sqrt(5./24.)*Mc**(5./6.)*f0**(-7./6.)/np.pi**(2./3)/Dl
+
+	if (f < f0):
+		A *= (f/f0)**(-7./6.)
+
+	elif (f0 <= f and f < f1):
+		A *= (f/f0)**(-2./3.)
+
+	elif (f1 <= f and f < f3):
+		w = 0.5*np.pi*f2*(f0/f1)**(2./3.)
+		A *= w*f2/((f - f1)**2 + 0.25*f2**2)/(2.*np.pi)
+	
+	else:
+		A *= 0.
+
+	return A
+
+def get_Dl(z, Omega_m, H0):
     """ calculate luminosity distance in geometrized units """
     # see http://arxiv.org/pdf/1111.6396v1.pdf
     x0 = (1. - Omega_m)/Omega_m
@@ -167,25 +176,60 @@ def get_Dl(z):
     PhiZ /= (1. + 1.392*xZ + 0.5121*xZ**2  + 0.03944*xZ**3)
     
     return 2.*C/H0*(1.0e-3*MPC)*(1. + z)/np.sqrt(Omega_m)*(Phi0 - PhiZ/np.sqrt(1. + z))
+   # return 2./H0*(1.0e-3*MPC)*(1. + z)/np.sqrt(Omega_m)*(Phi0 - PhiZ/np.sqrt(1. + z))
 
-def get_z(z, Dl):
+
+def get_z(z, Dl, Omega_m, H0):
     """ calculate redishift uisng root finder """
-    return get_Dl(z) - Dl
+    return get_Dl(z, Omega_m, H0) - Dl
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+def get_h_char_track(f, f_start, f_end, M, eta, M_chirp, Dl, constants):
 
+	# constants = np.arrays([H0, Omega_m, L, f_star, Tobs, NC])
+	f_star = constants[3]
+	L      = constants[2]
+	NC     = constants[5]
+	Tobs   = constants[4]
+	
+	A_arr   = np.zeros(len(f))
+	h_c_arr = np.zeros(len(f))
+
+	arg_start = np.where(f<=f_start)[0][-1]
+	if (f_end > 1.): # i.e. off the graph
+		arg_end = np.len(f)-1
+	else:
+		arg_end = np.where(f>=f_end)[0][0]
+
+	for i in range(arg_start, arg_end):
+		A_arr[i]   = get_A(f[i], M, eta, M_chirp, Dl) 
+		h_c_arr[i] = f[i]*A_arr[i]*np.sqrt(16./5.) 
+		
+		
+	SNR = 0.
+	Sn = get_Sn(f, constants)
+	for i in range(arg_start, arg_end):
+		freq   = 0.5*(f[i] + f[i-1])
+		Sn_est = 0.5*(1./Sn[i] + 1./Sn[i-1])
+		SNR += 16./5.*freq*get_A(freq, M, eta, M_chirp, Dl)**2*Sn_est*(np.log(f[i]) - np.log(f[i-1]))
+	SNR = np.sqrt(SNR)
+
+	return h_c_arr, SNR
     
     
-    
-    
+def get_h_char_point(f, f_start, f_end, M, eta, M_chirp, Dl, constants):
+
+	# constants = np.arrays([H0, Omega_m, L, f_star, Tobs, NC])
+	f_star = constants[3]
+	L      = constants[2]
+	NC     = constants[5]
+	Tobs   = constants[4]
+	
+	h_c = np.sqrt(16./5.*(f_end-f_start)*f_start)*get_A(f_start, M, eta, M_chirp, Dl)
+
+	Sn_est = get_Sn_approx(f_start, f_star, L, NC) + get_Sc_est(f_start, Tobs, NC)
+	SNR = 8.*np.sqrt(Tobs/5.)*M_chirp**(5./3.)*(np.pi*f_start)**(2./3.)/Dl/np.sqrt(Sn_est)
+        
+	return h_c, SNR
     
     
     
